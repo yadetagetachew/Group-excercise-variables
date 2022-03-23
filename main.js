ffbdd0553c451f293b48bed8a13cd311c6ce1766
Yadeta Getachew <86324546+yadetagetachew@users.noreply.github.com>
2022-03-23 13:20:23 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let name = "Yadi";
+let dayOfBirth = "11/18/1994";
+
+
+let myFavoriteColor = prompt("Please enter your favorite color","Purple");
+console.log("My favorite color is" + " " + myFavoriteColor + " " + "too!");
+
+
+    
+
+
+let quote = 'Dont worry be happy "Bob Marley"';
+
+console.log(quote);
+
+
+
